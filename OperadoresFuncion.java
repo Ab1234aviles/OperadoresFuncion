e6417b4510fa4387211a145e255086b342b7720d
Ana Belén Fernandez-Avilés Ruiz <95765527+Ab1234aviles@users.noreply.github.com>
2022-01-03 16:46:38 +0100

    Add files via upload

---
+++ OperadoresFuncion.java
@@ -0,0 +1,16 @@
+package Exercise2;
+
+public class OperadoresFuncion {
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int c;
+        a= 12000;
+        b= a*21/100;
+        c= a+b;
+
+        System.out.println("la suma de a y b es" + c);
+
+    }
+}
